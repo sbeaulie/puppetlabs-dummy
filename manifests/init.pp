fb6ec107bbfe999300933f4771f81988eaca7215
Justin Stoller <justin.stoller@gmail.com>
2014-09-24 20:37:31 -0700

    fail link checks

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,10 @@
 class dummy( $aparam = 'something' ) {
   notify { 'Hello World': }
-  $aVar = 'test$varsInSingleQuotedString'
-  notify { $aVar: }
+  $this_var = 'nothing'
+  $aVar = '${this_var}-sInSingleQuotedString'
+  notify { $aVar:
+   require => Notify['Hello World'],
+   message  => $aVar
+  }
+  notify { "this is a string one, could call it text, that is definitely equal to or longer than eighty characters... causing this line to fail standard puppet-lint checks": }
 }
